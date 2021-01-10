96436d0a51b5dfaa1c71c8151df382c419de980b
Salweyar Patel <spatel53@rrc.ca>
2021-01-10 12:41:08 -0600

    Created template for Login and Signup auth by using Firebase

---
+++ src/Login.js
@@ -0,0 +1,238 @@
+import React, { Component } from "react";
+import fire from "./config/fire";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  login(e) {
+    e.preventDefault();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((u) => {
+        console.log(u);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  signup(e) {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((u) => {
+        console.log(u);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <form>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            placeholder="Enter email address"
+            onChange={this.handleChange}
+            value={this.state.email}
+          />
+          <input
+            name="password"
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+          <button onClick={this.login}>Login</button>
+          <button onClick={this.signup}>Signup</button>
+        </form> */}
+
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label
+                    for="email"
+                    className="block text-sm font-medium text-gray-700 text-left mb-2"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="email"
+                      type="email"
+                      id="email"
+                      placeholder="Enter email address"
+                      onChange={this.handleChange}
+                      value={this.state.email}
+                      autocomplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    for="password"
+                    className="block text-sm font-medium text-gray-700 text-left mb-2"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="password"
+                      type="password"
+                      id="password"
+                      placeholder="Enter password"
+                      onChange={this.handleChange}
+                      value={this.state.password}
+                      autocomplete="current-password"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember_me"
+                      name="remember_me"
+                      type="checkbox"
+                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    />
+                    <label
+                      for="remember_me"
+                      className="ml-2 block text-sm text-gray-900"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="button"
+                    onClick={this.login}
+                    className="group relative w-full flex justify-center py-2 px-4 border  text-sm font-medium rounded text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                      <svg
+                        className="h-5 w-5 text-white group-hover:text-blue-1000"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    Log in
+                  </button>
+                </div>
+              </form>
+
+              <div className="mt-6">
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-300"></div>
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <span className="px-2 bg-white text-gray-500">
+                      New to Salweyar's template?
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6 border border-8 border-double border-gray-400 rounded-md ">
+                <button
+                  type="submit"
+                  className="group text-gray-600 relative w-full flex justify-center py-2 px-4  text-sm font-medium  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Create your account
+                </button>
+              </div>
+
+              <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mt-4">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <svg
+                      className="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm text-yellow-700">
+                      Please provide you email and password before clicking on
+                      "Create your account"!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
